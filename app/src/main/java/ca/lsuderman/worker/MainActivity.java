5d8dca4163a50ef14b6afdcca7bdb8f3e661cba2
Leighton <ljsuderman7@gmail.com>
2022-07-31 16:28:44 -0400

    added a periodic worker

--- app/src/main/java/ca/lsuderman/worker/MainActivity.java
+++ app/src/main/java/ca/lsuderman/worker/MainActivity.java
@@ -2,6 +2,7 @@ package ca.lsuderman.worker;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.work.OneTimeWorkRequest;
+import androidx.work.PeriodicWorkRequest;
 import androidx.work.WorkManager;
 import androidx.work.WorkRequest;
 
@@ -13,15 +14,15 @@ import java.util.concurrent.TimeUnit;
 
 public class MainActivity extends AppCompatActivity {
 
-    private Button btnSingleWorker;
+    private Button btnSingleWorker, btnPeriodicWorker;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        // SINGLE WORKER
         btnSingleWorker = findViewById(R.id.btnSingleWorker);
-
         btnSingleWorker.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -29,6 +30,18 @@ public class MainActivity extends AppCompatActivity {
                         .setInitialDelay(5, TimeUnit.SECONDS)
                         .build();
                 WorkManager.getInstance().enqueue(myWorkRequest);
+            }
+        });
+
+        // PERIODIC WORKER
+        btnPeriodicWorker = findViewById(R.id.btnPeriodicWorker);
+        btnPeriodicWorker.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                PeriodicWorkRequest request =
+                        new PeriodicWorkRequest.Builder(SingleWorker.class, 5, TimeUnit.SECONDS)
+                                .build();
+                WorkManager.getInstance().enqueue(request);
             }
         });
     }
